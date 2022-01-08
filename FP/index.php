7ee299e77d892edc88001c7e867c21f1d199bb2e
DESKTOP-RDNMC65\feria <feriapriliawan17@gmail.com>
2022-01-08 21:55:40 +0700

    no message

--- FP/index.php
+++ FP/index.php
@@ -80,6 +80,7 @@ include 'admin/config/db_conn.php';
          <button class="btn btn-primary btn-lg" type="button">Example button</button>
       </div>
 </div> -->
+
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -118,40 +119,7 @@ include 'admin/config/db_conn.php';
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-    <div id="carouselExampleControls" style="margin-top: 100px;" class="carousel slide" data-bs-ride="carousel">
 
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="foto/bp3.png" class="d-block w-100" width="100%" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="foto/VO4jJiY.png" class="d-block w-100" width="100%"  alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="foto/bts-desktop-nawpic-16-scaled.jpg" class="d-block w-100" width="100%" alt="...">
-            alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="foto/c5a16e3a8354af2739a151ba71577574.jpg" class="d-block w-100" width="100%" alt="...">
-            alt="">
-        </div>
-        <div class="carousel-item">
-          <img src="foto/ikon.png" class="d-block w-100" width="100%" alt="">
-        </div>
-
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-        data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-    
 <div class="container-fluid py-5">
 <div class="row">
     <div class="row">
@@ -185,9 +153,6 @@ include 'admin/config/db_conn.php';
     </div>
   </div>
 </div>
-
-
-
 
     <!-- Optional JavaScript; choose one of the two! -->
 
